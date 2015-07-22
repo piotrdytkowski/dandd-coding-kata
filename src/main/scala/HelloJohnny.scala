b3786bf2718a738fa241560a7bc138dac4b83e21
piotr.dytkowski <piotr.dytkowski@gmail.com>
2015-07-22 11:57:41 +0100

    first model class

---
+++ src/main/scala/HelloJohnny.scala
@@ -0,0 +1,6 @@
+object HelloJohnny {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello, Johnny!")
+  }
+}
---
+++ src/main/scala/com/betgenius/model/Item.scala
@@ -0,0 +1,7 @@
+package com.betgenius.model
+
+object Item extends Enumeration {
+  val Bone = Value("B")
+  val Coin = Value("C")
+  val Scroll = Value("S")
+}
